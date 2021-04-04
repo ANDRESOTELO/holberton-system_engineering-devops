e773021feb98b08a149d8a545db6d5301b8a7643
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-04 20:37:59 +0000

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Using puppet to create a file in /tmp
+
+file { '/tmp/holberton':
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
