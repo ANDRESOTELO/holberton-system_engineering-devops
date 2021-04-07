f0b40afd4687c3f6eae59fef7eb7d19e79b28b8a
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-07 05:01:36 +0000

    FIXED

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,22 +2,22 @@
 
 package { 'nginx':
   ensure   => 'present',
-  provider => 'apt',
+  provider => 'apt'
 }
 
 file { '/var/www/html/index.nginx-debian.html':
   ensure  => 'present',
-  content => 'Holberton School',
+  content => 'Holberton School'
 }
 
 file_line { 'Redirect_me':
   ensure => 'present',
   after  => 'listen 80 default_server;',
   path   => '/etc/nginx/sites-available/default',
-  line   => '	rewrite ^/redirect_me https://youtu.be/LNv9PevSRLo permanent;',
+  line   => '	rewrite ^/redirect_me https://youtu.be/LNv9PevSRLo permanent;'
 }
 
 service { 'service_start':
   ensure  => 'running',
-  require => Package['nginx'],
+  require => Package['nginx']
 }
