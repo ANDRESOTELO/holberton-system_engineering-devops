21f0fc7c64367b58aa2885cb06baffddd0266cf1
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-12 23:25:42 +0000

    ADDED | Puppet script to add_header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,32 @@
+# Install Nginx web server (w/ Puppet)
+
+package { 'nginx':
+  ensure   => installed,
+  name     => 'nginx',
+  provider => 'apt'
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => present,
+  content => 'Holberton School'
+}
+
+file_line { 'Redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => '	    rewrite ^/redirect_me https://youtu.be/LNv9PevSRLo permanent;'
+}
+
+file_line { 'custom_header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx']
+}
