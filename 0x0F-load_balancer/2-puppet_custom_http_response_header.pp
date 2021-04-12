5a24a6124c8e3bda1ee3c54097a3627064304b72
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-12 23:37:56 +0000

    ADDED | Puppet script to add_header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,19 +6,7 @@ package { 'nginx':
   provider => 'apt'
 }
 
-file { '/var/www/html/index.nginx-debian.html':
-  ensure  => present,
-  content => 'Holberton School'
-}
-
-file_line { 'Redirect_me':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => '	    rewrite ^/redirect_me https://youtu.be/LNv9PevSRLo permanent;'
-}
-
-file_line { 'custom_header':
+file_line { 'custom HTTP header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
