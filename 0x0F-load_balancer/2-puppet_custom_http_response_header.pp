335535d1bd566a0c5e0acf591764700dcca58b2d
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-12 23:39:34 +0000

    ADDED | Puppet script to add_header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,18 @@
-# Install Nginx web server (w/ Puppet)
+# Install Nginx web server
 
 package { 'nginx':
   ensure   => installed,
   name     => 'nginx',
   provider => 'apt'
 }
-
+->
 file_line { 'custom HTTP header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'add_header X-Served-By $hostname;'
 }
-
+->
 service { 'nginx':
   ensure  => running,
   enable  => true,
