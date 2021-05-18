2fce32d954042084cf01b8dded418633cc218cf3
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-18 01:15:03 +0000

    ADDED | Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,21 +8,21 @@ import requests
 def top_ten(subreddit):
     """Function that queries the Reddit API and prints the titles of
     the first 10 hot posts listed for a given subreddit"""
-    try:
-        url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-        request = requests.get(url,
-                               headers={'User-agent': 'myapp/1.0'},
-                               allow_redirects=False)
-        if request.status_code == 200:
-            response = request.json()
-            if response:
-                data = response.get('data')
-                children = data.get('children')
-
-                for child in children:
-                    children_data = child.get('data')
-                    print(children_data.get('title'))
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    request = requests.get(url,
+                           headers={'User-agent': 'myapp/1.0'},
+                           allow_redirects=False)
+    if request.status_code != 200:
         print(None)
         return None
+    try:
+        response = request.json()
+        if response:
+            data = response.get('data')
+            children = data.get('children')
+
+            for child in children:
+                children_data = child.get('data')
+                print(children_data.get('title'))
     except:
         print(None)
