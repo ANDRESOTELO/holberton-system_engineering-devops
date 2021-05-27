9dc834a3d89c3311b42f80a33e173d786081404e
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-27 15:39:55 +0000

    ADDED | puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Fix code Puppet
 
 exec { 'fix-ulimit':
-  command => 'sudo sed -i s/15/4096/g /etc/default/nginx',
+  command => 'sudo sed -i s/15/4096/ /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/:/usr/bin/env'
 }
 
--> exec { 'restart-service':
+->exec { 'restart-service':
   command => 'sudo service nginx restart',
   path    => '/usr/local/bin/:/bin/:/usr/bin/env'
 }
